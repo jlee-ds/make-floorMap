6b7ca582932d72c8d78ba235b2c5c918f8f9ecba
mymusese <mymusese@naver.com>
2016-07-23 14:23:06 +0900

    Update planeFromPoints.m

--- planeFromPoints.m
+++ planeFromPoints.m
@@ -1,14 +1,17 @@
 function [p] = planeFromPoints(p1,p2,p3)
 %find normal vector
 n = findNormalVector(p1,p2,p3);
+%n=[a,b,c]
 
 %d=-(ax+by+cz)
+%x <- p1(1), y <- p1(2), z <- p1(3)
 d = 0;
 for i = 1 : 3
     d = d + n(i) * p1(i);
 end
 d = d * (-1);
-p = [n(1),n(2),n(3),d]; 
+
+p = [n(1),n(2),n(3),d];
 p = round((p*100))/100;
 end
 
