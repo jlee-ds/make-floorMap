3fc778cb08dd8b2912926509336b892590b9c4b7
mymusese <mymusese@naver.com>
2016-07-23 15:30:23 +0900

    Update exampleFindPlane.m

--- exampleFindPlane.m
+++ exampleFindPlane.m
@@ -1,15 +1,14 @@
-%xyzFloorPoints = find_plane(xyzPoints, 0);
 xyzFloorPoints = findFloorPlane(xyzPoints, 0);
 figure(1);
 pcshow(xyzFloorPoints, 'VerticalAxis', 'y', 'VerticalAxisDir', 'down');
-% 표시되는 그래프의 라벨 이름을 설정.
+%set tne name of graph label
 xlabel('X (m)');
 ylabel('Y (m)');
 zlabel('Z (m)');
 
 figure(2);
 pcshow(xyzPoints, 'VerticalAxis', 'y', 'VerticalAxisDir', 'down');
-% 표시되는 그래프의 라벨 이름을 설정.
+%set tne name of graph label
 xlabel('X (m)');
 ylabel('Y (m)');
 zlabel('Z (m)');
