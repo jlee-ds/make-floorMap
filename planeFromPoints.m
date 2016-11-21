a5c3a96bdb3a637ba3b3a352629bc1e54e6fffe9
jlee-ds <jwonlee206@gmail.com>
2016-11-21 16:17:07 +0900

    Update planeFromPoints.m

--- planeFromPoints.m
+++ planeFromPoints.m
@@ -1,3 +1,9 @@
+% 2016. 11. 17. made by Jongwon Lee and Soree Choi.
+% Hanyang Uni. Last project for graduation.
+% url: [https://github.com/jlee-ds/makeFloorMap]
+
+% This code is to find a plane using three 3d points.
+
 function [p] = planeFromPoints(p1,p2,p3)
 %find normal vector
 n = findNormalVector(p1,p2,p3);
@@ -14,4 +20,3 @@ d = d * (-1);
 p = [n(1),n(2),n(3),d];
 p = round((p*100))/100;
 end
-
