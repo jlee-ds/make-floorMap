f59877bbd2a55a17c48222e86a304b8504fac292
jlee-ds <jwonlee206@gmail.com>
2016-11-21 16:15:16 +0900

    Update example3dPoint.m

--- example3dPoint.m
+++ example3dPoint.m
@@ -1,30 +1,32 @@
-%basic example of 3D point
-%show 3D coordinate transformed from raw depth data when executed
+% 2016. 11. 17. made by Jongwon Lee and Soree Choi.
+% Hanyang Uni. Last project for graduation.
+% url: [https://github.com/jlee-ds/makeFloorMap]
 
-%the number of Kinect depth sensor is 2
-%create object according to device
+% This code is to get 3d points from kinect depth sensor.
+% and to show the 3d points.
+
+% the number of Kinect depth sensor is 2
+% create object according to device
 depthDevice = imaq.VideoDevice('kinect', 2);
 
-%create tab to show values of depth sensors
-preview(depthDevice);
-
-%call 'step' for warming(don't know why)
+% call 'step' for warming(don't know why)
 frame = step(depthDevice);
 
-%store raw depth data at a moment to depthImage with 'step'
+% store raw depth data at a moment to depthImage with 'step'
 depthImage = step(depthDevice);
 
-%transform data to 3D coordinate then store it to an array which size is
-%640*480*3
+% transform data to 3D coordinate then store it to an array which size is
+% 640*480*3
 xyzPoints = depthToPointCloud(depthImage, depthDevice);
 
-%don't know why
+% show 3D points
+figure(1);
 pcshow(xyzPoints, 'VerticalAxis', 'y', 'VerticalAxisDir', 'down');
 
-%set the name of graph label
+% set the name of graph label
 xlabel('X (m)');
 ylabel('Y (m)');
 zlabel('Z (m)');
 
-%delete object
+% delete object
 release(depthDevice); 
