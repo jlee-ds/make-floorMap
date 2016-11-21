1818b9f0cb8d02d7c88e610c466f01bf00d5c666
jlee-ds <jwonlee206@gmail.com>
2016-11-21 16:16:45 +0900

    Update findNormalVector.m

--- findNormalVector.m
+++ findNormalVector.m
@@ -1,3 +1,10 @@
+% 2016. 11. 17. made by Jongwon Lee and Soree Choi.
+% Hanyang Uni. Last project for graduation.
+% url: [https://github.com/jlee-ds/makeFloorMap]
+
+% This code is to find normal vector of 3 points.
+% exactly of a plane composed with 3 points.
+
 function [n] = findNormalVector(p1,p2,p3)
     for i=1:3
         v1(i)=p2(i)-p1(i); %v1 is a vector from p1 to p2
@@ -6,4 +13,3 @@ function [n] = findNormalVector(p1,p2,p3)
     n = cross(v1,v2); %cross vector v1 and v2
     n = n / norm(n); %make n as an unit vector
 end
-
